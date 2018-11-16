6669bd6f161ce448dfbce8a663b85c929f5ad91e
Milner97 <36887534+Milner97@users.noreply.github.com>
2018-11-16 16:03:02 +0000

    Add files via upload

--- SE Program.py
+++ SE Program.py
@@ -30,39 +30,44 @@ class MovieDBApplication(Frame):
         SearchLabel.grid(row = 2, column = 1)
         SearchEntry = Entry(self.SearchFrame, font = ("Arial", 12))
         SearchEntry.grid(row = 2, column = 2, padx = 10)
-        GoButton = Button(self.SearchFrame, text = "Go", font = ("Arial", 12, "bold"), command = lambda:self.SearchFilm(SearchEntry)) #add command
+        GoButton = Button(self.SearchFrame, text = "Go", font = ("Arial", 12, "bold"), command = lambda:self.SearchFilm(SearchEntry))
         GoButton.grid(row = 2, column = 3)
         self.SearchFrame.grid()
 
 
-    def SearchFilm(self, title):
+    def SearchFilm(self, SearchEntry):
+        #Get data from the api
         api = 'http://www.omdbapi.com/?'
         key = '&apikey=e0ad0310'
-        url = api + urllib.parse.urlencode({'t': title.get()}) + key
-
+        url = api + urllib.parse.urlencode({'t': SearchEntry.get()}) + key
         data = requests.get(url).json()
 
+        #Put data into a readable string with line spaces
         filmInfo = ""
         for each in data:
             try:
                 filmInfo = filmInfo + str(each + ": " + data[each]) + "\n"
             except:
                 pass
-        #print(filmInfo)
+
+        #Set display to use for textvariable meaning it can change when reopened
         display.set(filmInfo)
+        #Display in results frame
         InfoLabel = Label(self.ResultsFrame, textvariable = display, font = ("Arial", 12))
         InfoLabel.grid(row = 2, column = 1, columnspan = 3, padx = 5)
-        
+        #Clear search bar
+        SearchEntry.delete(0,END)
         self.SearchFrame.grid_remove()
         self.ResultsFrame.grid()
         
-
+    #Create Results frame widgets
     def CreateResultsWidgets(self, filmInfo):
         ResTitleLabel = Label(self.ResultsFrame, text = "ACME Movie Database", font = ("Arial", 30, "bold"))
         ResTitleLabel.grid(row = 1, column = 1, columnspan = 3, pady = 10)
         BackButton = Button(self.ResultsFrame, text = "Back", font = ("Arial",12, "bold"), command = lambda:self.BackToSearch())
         BackButton.grid(row = 3, column = 2)
-        
+
+    #removes results frame and displays search frame
     def BackToSearch(self):
         self.ResultsFrame.grid_remove()
         self.SearchFrame.grid()
